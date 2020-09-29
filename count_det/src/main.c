b10d8473fa7f22fd6c73485f99c72a64236882de
Slava <vsvoiteshonok1@gmail.com>
2020-09-29 11:07:57 +0300

    hw_2 added

--- count_det/src/main.c
+++ count_det/src/main.c
@@ -5,18 +5,17 @@
 
 int32_t count_det (int32_t* numbers);
 
-int main () {
+int main (){
 	int32_t* numbers = malloc(sizeof(int32_t) * 9);
-	
+
 	for (uint8_t i = 0; i < 9; ++ i)
 		scanf("%d", &numbers[i]);
 
 	const int32_t det = count_det(numbers);
 
-	printf("%d\n", det);
+	printf("%d", det);
 
 	free(numbers);
 
 	return 0;
 }
-
